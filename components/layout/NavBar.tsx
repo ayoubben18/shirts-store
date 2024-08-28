e326092653880522ea27e4b59c3f8e1c740f6434
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-08-28 01:06:46 +0100

    fix:hide checkout button on rediraction

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -3,11 +3,15 @@ import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
+import { useShirtStore } from "@/stores/useShirtsStore";
 
 const NavBar = () => {
   const router = useRouter();
+  const { shirts } = useShirtStore();
   return (
-    <div className="container mx-auto flex items-center justify-between px-4">
+    <div
+      className={`${shirts.length === 0 && "hidden"} container mx-auto flex items-center justify-between px-4`}
+    >
       <div className="flex h-20 items-center justify-between">
         <Link href={`/`} className="text-3xl font-black">
           <HomeIcon className="h-8 w-8" />
