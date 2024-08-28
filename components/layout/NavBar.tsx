a878ee2dab01ede104d57f7ed841a7cb746d2260
ayoubben18 <ayoubbensalah2004@gmail.com>
2024-08-28 01:12:59 +0100

    fix:

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -9,15 +9,18 @@ const NavBar = () => {
   const router = useRouter();
   const { shirts } = useShirtStore();
   return (
-    <div
-      className={`${shirts.length === 0 && "hidden"} container mx-auto flex items-center justify-between px-4`}
-    >
+    <div className={`container mx-auto flex items-center justify-between px-4`}>
       <div className="flex h-20 items-center justify-between">
         <Link href={`/`} className="text-3xl font-black">
           <HomeIcon className="h-8 w-8" />
         </Link>
       </div>
-      <Button onClick={() => router.push("/informations")}>Checkout</Button>
+      <Button
+        onClick={() => router.push("/informations")}
+        className={`${shirts.length === 0 && "hidden"}`}
+      >
+        Checkout
+      </Button>
     </div>
   );
 };
